6dc74c37a1714b65429c2cdf51dee34598b1b9b6
Thales Catão <34405260+thalescatao@users.noreply.github.com>
2026-05-17 17:18:55 -0300

    bug

--- nav.js
+++ nav.js
@@ -245,7 +245,7 @@
             revealObserver.unobserve(entry.target);
           }
         });
-      }, { threshold: 0.1, rootMargin: '0px 0px -40px 0px' });
+      }, { threshold: 0.1, rootMargin: '0px 0px 80px 0px' });
 
       revealEls.forEach(function (el) { revealObserver.observe(el); });
     } else {
